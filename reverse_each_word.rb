79f52e6b8a191cae9a2bb7dd0064e4b1b0da0ede
droll-accelerator-1483 <srsani@gmail.com>
2019-07-24 14:06:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word (string_in)
   s_out= s.split.reverse
   s_out_f = s_out.join(' ')
 end 
+
+def reverse_each_word (string_in)
+  s = ''
+  string_in.each_char do |str|
+    s = str+s
+  end
+  s_out= s.split.reverse
+  s_out_f = s_out.join(' ')
+end 
